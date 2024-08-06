24c1cb489023b000821f87e56e2124ffe104d465
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-08-07 04:06:19 +0530

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // BASE_API_PATH: "http://localhost:8000/mms/api/v1/",
   // BASE_FILE_PATH: "http://localhost:8000/uploads/",
-  BASE_API_PATH: "http://3.12.161.14/mms/api/v1/",
-  BASE_FILE_PATH: "http://3.12.161.14/uploads/",  
+  BASE_API_PATH: "http://3.147.48.85/backend/mms/api/v1/",
+  BASE_FILE_PATH: "http://3.147.48.85/backend/uploads/",  
   production: false
 };
