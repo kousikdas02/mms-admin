834314450bfc0e7d9730e40ddea524c2b4621e7b
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-08-07 03:09:11 +0530

    live url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  BASE_API_PATH: "http://localhost:8000/mms/api/v1/",
-  BASE_FILE_PATH: "http://localhost:8000/uploads/",
+  // BASE_API_PATH: "http://localhost:8000/mms/api/v1/",
+  // BASE_FILE_PATH: "http://localhost:8000/uploads/",
+  BASE_API_PATH: "http://3.12.161.14/mms/api/v1/",
+  BASE_FILE_PATH: "http://3.12.161.14/uploads/", 
   production: false
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
+  BASE_API_PATH: "http://3.12.161.14/mms/api/v1/",
+  BASE_FILE_PATH: "http://3.12.161.14/uploads/", 
   production: true
 };
