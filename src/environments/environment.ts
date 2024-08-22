eba5d0aadfb6858ef1f26ca19a921f7b6efe918a
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-08-23 02:52:58 +0530

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  BASE_API_PATH: "http://backend.mmstechnicians.com/mms/api/v1/",
-  BASE_FILE_PATH: "http://backend.mmstechnicians.com/uploads/",  
+  BASE_API_PATH: "https://backend.mmstechnicians.com/mms/api/v1/",
+  BASE_FILE_PATH: "https://backend.mmstechnicians.com/uploads/",  
   production: false
 };
